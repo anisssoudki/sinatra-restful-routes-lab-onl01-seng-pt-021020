80263d5a4c850aa49d209cb2753f14a7494dd1e7
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-22 19:18:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
     erb :edit
   redirect to '/recipes'
   end
